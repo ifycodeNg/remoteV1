0016712e24374a397b32b0c096a4729b4d7a14a8
ifycodeng01 <ifeanyi.ugwumba@queryfier.com>
2024-03-09 12:31:50 +0100

    Add tailwindcss config

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,7 @@
-import { useState } from 'react'
-
-import './App.css'
-
-function App() {
-
+export default function App() {
   return (
-  
-    <div > Welcome to Remote working </div>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
   )
 }
-
-export default App
